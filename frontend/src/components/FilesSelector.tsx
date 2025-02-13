52f904c9466f4671afdb6736ae57b9369ecee501
Alfonso Enciso <198062495+encisoa-informed@users.noreply.github.com>
2025-02-13 13:24:06 +0000

    feat(DPAV-216): Refactor FilesSelector.tsx to fix Non-interactive DOM elements should not have an interactive handler

--- frontend/src/components/FilesSelector.tsx
+++ frontend/src/components/FilesSelector.tsx
@@ -2,11 +2,11 @@ import { FormEvent, DragEvent, useRef, useLayoutEffect, useState } from 'react';
 
 type SelectorProps = {
   onSelect: (files: File[]) => void;
-}
+};
 
 const TYPES_BLACKLIST: string[] = [
   'application/x-msdownload',
-  'application/x-apple-diskimage'
+  'application/x-apple-diskimage',
 ];
 
 export default function FilesSelector({ onSelect }: Readonly<SelectorProps>) {
@@ -14,39 +14,50 @@ export default function FilesSelector({ onSelect }: Readonly<SelectorProps>) {
   const [focused, setFocused] = useState<boolean>(false);
   const [isDragging, setIsDragging] = useState<boolean>(false);
 
-  const dragContainerRef = useRef<HTMLDivElement>(null);
+  // We use the same button as the drop target
+  const dragContainerRef = useRef<HTMLButtonElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
-    if (!dragContainerRef?.current) {
+    if (!dragContainerRef.current) {
       return;
     }
-    const dropDragSupported = (('draggable' in dragContainerRef.current) || ('ondragstart' in dragContainerRef.current && 'ondrop' in dragContainerRef.current))
-      && 'FormData' in window && 'FileReader' in window;
-    setCanDrag(dropDragSupported);
+    const dropSupported = ('draggable' in dragContainerRef.current
+        || ('ondragstart' in dragContainerRef.current && 'ondrop' in dragContainerRef.current))
+      && 'FormData' in window
+      && 'FileReader' in window;
+    setCanDrag(dropSupported);
   }, []);
 
-  const onDragOver = (evt: DragEvent<HTMLDivElement>) => {
-    // ensures we get the ondrop firing by stopping the browser default behaviour
+  const onDragOver = (evt: DragEvent<HTMLButtonElement>) => {
     evt.preventDefault();
     evt.stopPropagation();
   };
 
-  const onDragEnter = (evt: DragEvent<HTMLDivElement>) => {
+  const onDragEnter = (evt: DragEvent<HTMLButtonElement>) => {
     evt.preventDefault();
     evt.stopPropagation();
     setIsDragging(true);
   };
 
-  const onDragLeave = (evt: DragEvent<HTMLDivElement>) => {
+  const onDragLeave = (evt: DragEvent<HTMLButtonElement>) => {
     evt.preventDefault();
     evt.stopPropagation();
     setIsDragging(false);
   };
 
   const handleFiles = (fileList: FileList | null) => {
-    const files = Array.from(fileList || [])
-      .filter((file) => file.type && !TYPES_BLACKLIST.includes(file.type));
+    const files = Array.from(fileList || []).filter(
+      (file) => file.type && !TYPES_BLACKLIST.includes(file.type)
+    );
     onSelect(files);
+  };
+
+  const onFilesDrop = (evt: DragEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    setIsDragging(false);
+    handleFiles(evt.dataTransfer.files);
   };
 
   const onFileSelect = (evt: FormEvent<HTMLInputElement>) => {
@@ -54,35 +65,40 @@ export default function FilesSelector({ onSelect }: Readonly<SelectorProps>) {
     handleFiles(evt.currentTarget.files);
   };
 
-  const onFilesDrop = (evt: DragEvent<HTMLDivElement>) => {
-    evt.preventDefault();
-    evt.stopPropagation();
-    setIsDragging(false);
-    handleFiles(evt.dataTransfer.files);
+  // onClick triggers the file input
+  const onClick = () => {
+    fileInputRef.current?.click();
   };
 
   return (
-    <div
-      className={`files-selector ${isDragging ? 'files-selector__dragging' : ''}`}
-      ref={dragContainerRef}
-      onDragOver={onDragOver}
-      onDragEnter={onDragEnter}
-      onDragLeave={onDragLeave}
-      onDrop={onFilesDrop}
-    >
-      <label htmlFor="fileUpload">
-        <span className={`prompt ${focused ? 'prompt__focused' : ''}`}>Choose files</span>
-        {canDrag && ' or drag them here'}
-      </label>
+    <>
+      <button
+        type="button"
+        className={`files-selector ${isDragging ? 'files-selector__dragging' : ''}`}
+        ref={dragContainerRef}
+        aria-label="Choose files or drag them here"
+        onClick={onClick}
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onFilesDrop}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      >
+        <label htmlFor="fileUpload">
+          <span className={`prompt ${focused ? 'prompt__focused' : ''}`}>Choose files</span>
+          {canDrag && ' or drag them here'}
+        </label>
+      </button>
+      {/* Hidden file input */}
       <input
         id="fileUpload"
         type="file"
         multiple
         onChange={onFileSelect}
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(false)}
-        value=""
+        ref={fileInputRef}
+        style={{ display: 'none' }}
       />
-    </div>
+    </>
   );
 }
