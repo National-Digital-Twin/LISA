930f626fff0e85e6775ad0706af91c0dcbd643fd
KumailKamranIS <131384933+KumailKamranIS@users.noreply.github.com>
2025-02-25 18:59:11 +0000

    fix(dpav-555): fixed web socket and sign out integration (#37)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,26 +4,16 @@ import { createServer } from 'http';
 // Local imports
 import app from './app';
 import { handleUpgrade } from './pubSub/server';
-import { getUserDetailsForWs } from './services/auth';
 import { settings } from './settings';
 
 const server = createServer(app);
 server.on('upgrade', async (request, socket, head) => {
-  let user;
   if (request.url !== '/api/ws') {
     socket.destroy();
     return;
   }
-  try {
-    user = await getUserDetailsForWs(request, socket);
-  } catch (error) {
-    console.log('Error fetching user details', error);
-    socket.destroy();
-  }
 
-  if (user?.email && user?.username) {
-    await handleUpgrade(request, socket, head, user);
-  }
+  await handleUpgrade(request, socket, head);
 });
 
 server.listen(settings.PORT, settings.HOST, () => {
