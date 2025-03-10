b84954a7aa93b5de2fb9a29f6c9bb1e46906a848
Sam Hill <sam.hill@informed.com>
2025-03-10 14:50:55 +0000

    more changes

--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -18,7 +18,8 @@ export const settings = cleanEnv(process.env, {
   }),
   IDENTITY_API_URL: str({ default: 'http://localhost:3001' }),
   LANDING_PAGE_URL: str({ default: 'http://localhost:3002' }),
-  COGNITO_USER_GROUP_NAME: str()
+  COGNITO_USER_GROUP_NAME: str(),
+  OS_MAPS_KEY: str()
 });
 
 const filename = fileURLToPath(import.meta.url); // get the resolved path to the file
