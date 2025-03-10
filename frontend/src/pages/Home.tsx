21a524f2af249c5c7627d989b5def44f11d87640
KumailKamranIS <131384933+KumailKamranIS@users.noreply.github.com>
2025-03-10 17:56:09 +0000

    fix(non-req): remove incident id from the home page (#73)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -57,9 +57,7 @@ const Home = () => {
 
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const tableHeaders = isMobile
-    ? ['ID', 'Incident']
-    : ['Incident ID', 'Incident name', 'Reported by', 'Date', 'Stage'];
+  const tableHeaders = isMobile ? ['Incident'] : ['Incident name', 'Reported by', 'Date', 'Stage'];
 
   return (
     <PageWrapper>
@@ -125,7 +123,6 @@ const Home = () => {
               const { id, name, reportedBy, startedAt } = incident;
               return isMobile ? (
                 <TableRow key={id}>
-                  <TableCell>{id}</TableCell>
                   <TableCell width="70%">
                     <Box display="flex" flexDirection="column" gap="0.3rem">
                       <Typography
@@ -151,7 +148,6 @@ const Home = () => {
                 </TableRow>
               ) : (
                 <TableRow key={id}>
-                  <TableCell>{id}</TableCell>
                   <TableCell>
                     <Typography
                       component={Link}
