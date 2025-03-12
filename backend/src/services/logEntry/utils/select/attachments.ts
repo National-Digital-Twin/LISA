266f792e3988af04c26704347404688ff7ed59bb
Owen Lambert <owen.lambert@informed.com>
2025-03-12 16:25:34 +0000

    feat: [DPAV-330] Enable auth against the IA

--- backend/src/services/logEntry/utils/select/attachments.ts
+++ backend/src/services/logEntry/utils/select/attachments.ts
@@ -1,9 +1,12 @@
+// Global imports
+import { Request } from 'express';
+
 // Local imports
 import { select } from '../../../../ia';
 import { ns } from '../../../../rdfutil';
 
-export function attachments(incidentId: string) {
-  return select({
+export function attachments(req: Request, incidentId: string) {
+  return select(req, {
     clause: [
       [ns.data(incidentId), ns.lisa.hasLogEntry, '?entryId'],
       ['?entryId', ns.lisa.hasAttachment, '?attachmentId'],
--- backend/src/services/logEntry/utils/select/mentionsLogEntry.ts
+++ backend/src/services/logEntry/utils/select/mentionsLogEntry.ts
@@ -1,9 +1,12 @@
+// Global imports
+import { Request } from 'express';
+
 // Local imports
 import { select } from '../../../../ia';
 import { ns } from '../../../../rdfutil';
 
-export function mentionsLogEntry(incidentId: string) {
-  return select({
+export function mentionsLogEntry(req: Request, incidentId: string) {
+  return select(req, {
     clause: [
       [ns.data(incidentId), ns.lisa.hasLogEntry, '?entryId'],
       ['?mentionsLogEntry', ns.lisa.isMentionedBy, '?entryId']
