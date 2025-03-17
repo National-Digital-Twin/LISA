0b0a5aff25a74e4a919bee9a67dc6ac7bbf32eed
KumailKamranIS <131384933+KumailKamranIS@users.noreply.github.com>
2025-03-17 09:27:09 +0000

    feat(dpav-333): added av scan result flow (#112)

--- backend/src/services/logEntry/utils/attachments/parseAttachments.ts
+++ backend/src/services/logEntry/utils/attachments/parseAttachments.ts
@@ -2,19 +2,22 @@
 import { LogEntryAttachmentType, type LogEntryAttachment } from 'common/LogEntryAttachment';
 import { type ResultRow } from '../../../../ia';
 import { nodeValue } from '../../../../rdfutil';
+import { getScanResultInternal } from '../../../fileStorage';
 
-export function parseAttachments(results: ResultRow[]) {
-  return results.reduce((map, result) => {
+export async function parseAttachments(results: ResultRow[]) {
+  return results.reduce(async (map, result) => {
     const entryId = nodeValue(result.entryId.value);
     const attachment: LogEntryAttachment = {
       type: result.attachmentType.value as LogEntryAttachmentType,
       name: result.attachmentName.value,
       key: result.attachmentKey.value,
       mimeType: result.attachmentMimeType.value,
-      size: Number(result.attachmentSize.value)
+      size: Number(result.attachmentSize.value),
+      scanResult: await getScanResultInternal(result.attachmentKey.value)
     };
-    const attachments = map[entryId] || [];
+    const resolvedMap = await map;
+    const attachments = resolvedMap[entryId] || [];
     attachments.push(attachment);
-    return { ...map, [entryId]: attachments };
-  }, new Map<string, LogEntryAttachment[]>());
+    return { ...resolvedMap, [entryId]: attachments };
+  }, Promise.resolve(new Map<string, LogEntryAttachment[]>()));
 }
--- frontend/src/utils/Format/lexical/lexicalNode.ts
+++ frontend/src/utils/Format/lexical/lexicalNode.ts
@@ -10,15 +10,15 @@ import { paragraph } from './paragraph';
 
 export function lexicalNode(node: Node, key: Key): ReactNode {
   switch (node.type) {
-  case 'text':
-    return text(node, key);
-  case 'paragraph':
-    return paragraph(node, key, lexicalNode);
-  case 'mention':
-    return mention(node, key);
-  case 'linebreak':
-    return linebreak(key);
-  default:
-    return null;
+    case 'text':
+      return text(node, key);
+    case 'paragraph':
+      return paragraph(node, key, lexicalNode);
+    case 'mention':
+      return mention(node, key);
+    case 'linebreak':
+      return linebreak(key);
+    default:
+      return null;
   }
 }
