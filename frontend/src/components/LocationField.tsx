128f8da80b7ffa21c4153874d460c3943ca300f7
James <james.stephens@informed.com>
2025-03-18 11:15:32 +0000

    fix(DPAV-652): Location field error message not displaying.

--- frontend/src/components/LocationField.tsx
+++ frontend/src/components/LocationField.tsx
@@ -2,13 +2,15 @@ import { Button, InputAdornment, TextField } from '@mui/material';
 // Local imports
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { LocationTypes } from 'common/Location';
+import { ValidationError } from '../utils/types';
 
 type Props = {
   id: string;
   value: string | undefined;
   onClick: () => void;
+  error?: ValidationError;
 };
-export default function LocationField({ id, value, onClick }: Readonly<Props>) {
+export default function LocationField({ id, value, onClick, error }: Readonly<Props>) {
   return (
     <TextField
       data-testid="location-field"
@@ -17,6 +19,8 @@ export default function LocationField({ id, value, onClick }: Readonly<Props>) {
       fullWidth
       variant="filled"
       value={value === 'View location' ? LocationTypes.coordinates : value}
+      error={Boolean(error)}
+      helperText={error?.error}
       slotProps={{
         input: {
           endAdornment: (
