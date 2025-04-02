4ac1febdf6154e39f5e7cfc550c248c7f6001c5c
Sam Hill <sam.hill@informed.com>
2025-04-02 12:40:54 +0100

    Add licensing + remove YesNoBoolean control

--- frontend/src/hooks/useTasks.ts
+++ frontend/src/hooks/useTasks.ts
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
+// and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { type Task } from 'common/Task';
 // eslint-disable-next-line import/no-extraneous-dependencies
--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
+// and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
+
 import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Box, Button, Typography } from '@mui/material';
