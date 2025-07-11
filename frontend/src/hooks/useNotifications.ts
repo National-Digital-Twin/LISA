cf63adbebd892f425eff61d791c11490a52833b9
KumailKamranIS <kumail.kamran@informed.com>
2025-07-11 11:23:43 +0100

    bugfix(dpav-1148): added polling mechanism to fetch updates to notifications

--- frontend/src/hooks/useNotifications.ts
+++ frontend/src/hooks/useNotifications.ts
@@ -2,7 +2,7 @@
 // © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
 // and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
 
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { type Notification } from 'common/Notification';
 import { useCallback } from 'react';
 
@@ -25,14 +25,90 @@ export function useNotifications() {
   return { notifications: data, isLoading, isError, error, invalidate };
 }
 
-export function useReadNotification() {
-  const queryClient = useQueryClient();
-  const readNotification = useMutation<void, Error, string>({
-    mutationFn: (id: string) => put(`/notifications/${id}`, {}),
-    onSettled: async () =>
+export async function pollForTotalNotifications(
+  totalCurrentNotifications: number,
+  attemptNumber: number,
+  invalidateNotifications: () => void,
+  resetNewNotifications: () => void
+) {
+  const notifications = await get<Notification[]>('/notifications');
+
+  if (attemptNumber <= 10) {
+    if (notifications.length > totalCurrentNotifications) {
+      invalidateNotifications();
+      resetNewNotifications();
+    } else {
+      setTimeout(
+        () =>
+          pollForTotalNotifications(
+            totalCurrentNotifications,
+            attemptNumber + 1,
+            invalidateNotifications,
+            resetNewNotifications
+          ),
+        10000
+      );
+    }
+  } else {
+    invalidateNotifications();
+    resetNewNotifications();
+  }
+}
+
+async function pollForReadNotifications(
+  notificationId: string,
+  attemptNumber: number,
+  queryClient: QueryClient
+) {
+  const notifications = await get<Notification[]>('/notifications');
+
+  if (attemptNumber <= 10) {
+    if (
+      notifications.find((notification) => notification.id === notificationId && notification.read)
+    ) {
       queryClient.invalidateQueries({
         queryKey: ['notifications']
-      })
+      });
+    } else {
+      setTimeout(
+        () => pollForReadNotifications(notificationId, attemptNumber + 1, queryClient),
+        10000
+      );
+    }
+  } else {
+    queryClient.invalidateQueries({
+      queryKey: ['notifications']
+    });
+  }
+}
+
+export function useReadNotification() {
+  const queryClient = useQueryClient();
+  const readNotification = useMutation<{ id: string }, Error, string>({
+    mutationFn: (id: string) => put(`/notifications/${id}`, {}),
+    onSuccess: async (data) =>
+      setTimeout(() => pollForReadNotifications(data.id, 1, queryClient), 1000),
+    onMutate: async (id: string) => {
+      await queryClient.cancelQueries({ queryKey: ['notifications'] });
+      const notifications = queryClient.getQueryData<Notification[]>(['notifications']);
+
+      if (notifications) {
+        const otherNotifications: Notification[] = notifications!.filter(
+          (notification) => notification.id !== id
+        );
+        const notificationToBeUpdated: Notification | undefined = notifications!.find(
+          (notification) => notification.id === id
+        );
+
+        if (notificationToBeUpdated) {
+          const updatedNotifications: Notification[] = [
+            ...otherNotifications,
+            { ...notificationToBeUpdated, read: true }
+          ];
+          queryClient.setQueryData<Notification[]>(['notifications'], updatedNotifications);
+        }
+      }
+    }
   });
   return readNotification;
 }
