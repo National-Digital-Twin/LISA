0328794ed337736cbb9631004a6609171e7d4050
Andrew Cunliffe <andrew.cunliffe@informed.com>
2025-07-15 09:46:30 +0100

    fix(DPAV-1149): show log entries in log not just via toast click (#221)
    * fix(DPAV-1149): show log entries in log not just via toast
    
    * fix(DPAV-1149): use polling to load logs vs websocket trigger

--- frontend/src/hooks/useLogEntriesUpdates.tsx
+++ frontend/src/hooks/useLogEntriesUpdates.tsx
@@ -3,20 +3,22 @@
 // and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
 
 import { useQueryClient } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import useMessaging from './useMessaging';
 import { useToast } from './useToasts';
 
 export function useLogEntriesUpdates(incidentId: string) {
+  const pollingInterval = 10;
   const queryClient = useQueryClient();
   const [hasNewLogEntries, resetNewLogEntries] = useMessaging('NewLogEntries', incidentId);
   const postToast = useToast();
+  const pollingIntervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
   useEffect(() => {
-    if (!hasNewLogEntries) return;
+    if (!incidentId) return;
 
-    const handler = async () => {
+    const pollForNewEntries = async () => {
       await queryClient.invalidateQueries({
         queryKey: [`incident/${incidentId}/logEntries`]
       });
@@ -25,16 +27,26 @@ export function useLogEntriesUpdates(incidentId: string) {
       });
     };
 
+    pollingIntervalRef.current = setInterval(pollForNewEntries, pollingInterval * 1000);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      if (pollingIntervalRef.current) {
+        clearInterval(pollingIntervalRef.current);
+        pollingIntervalRef.current = null;
+      }
+    };
+  }, [incidentId, queryClient]);
+
+  useEffect(() => {
+    if (!hasNewLogEntries) return;
+
     postToast({
       type: 'Info',
       id: `NEW_LOG_ENTRIES:${incidentId}`,
       content: (
         <span>
-          New log entries have been added to this incident.{' '}
-          <button type="button" onClick={handler}>
-            Click here
-          </button>{' '}
-          to load them.
+          New log entries have been detected. Checking for updates every {pollingInterval} seconds...
         </span>
       ),
       isDismissable: true
