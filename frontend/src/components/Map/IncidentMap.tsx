8a13bdf3a5974b42ffbfc877d666c417f526b1ec
cruddasj <james.cruddas@informed.com>
2025-08-18 11:18:10 +0100

    fix(NON-REQ): map rendering fix

--- frontend/src/components/Map/IncidentMap.tsx
+++ frontend/src/components/Map/IncidentMap.tsx
@@ -3,10 +3,11 @@
 // and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
 
 // Global imports
+import 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import CloseIcon from '@mui/icons-material/Close';
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
 import { FitBoundsOptions } from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
 import { MouseEvent, ReactElement, useEffect, useMemo, useRef, useState } from 'react';
 import Map, { LngLatBoundsLike, MapRef, Marker, NavigationControl } from 'react-map-gl/maplibre';
 import { useNavigate } from 'react-router-dom';
